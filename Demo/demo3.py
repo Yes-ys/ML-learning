ffba361d22e2922f4c23b7c1ecfddf26276eba10
Yes-ys <2583777577@qq.com>
2026-03-09 15:38:14 +0800

    feat: 新增transform的使用demo

---
+++ Demo/demo3.py
@@ -0,0 +1,17 @@
+"""
+演示trasnform的使用
+"""
+
+from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.transforms import ToTensor
+
+pil_img = Image.open("/Users/yesys/Py_projects/MLpre/DataSets/hymenoptera_data/train/bees/17209602_fe5a5a746f.jpg")
+
+tensor_trans = ToTensor()
+
+tensor_img = tensor_trans(pil_img)
+
+writer = SummaryWriter("logs")
+writer.add_image("tensor_img", tensor_img)
+writer.close()
